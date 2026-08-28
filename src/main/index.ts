078a8aa07850ba77ad0e5c04087b6c0db903705f
timeflysoon <88620233+timeflysoon@users.noreply.github.com>
2026-08-28 12:12:55 +0800

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, dialog, shell } from 'electron'
+import { appendFileSync, mkdirSync } from 'node:fs'
 import { isAbsolute, join } from 'node:path'
 import { BrowserLauncher } from './browser-launcher'
 import { registerIpc } from './ipc'
